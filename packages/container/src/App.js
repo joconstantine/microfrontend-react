ac60447866426760accadd59e280628534f468fe
joconstantine <joconstantine@users.noreply.github.com>
2021-09-26 15:47:42 +0800

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MarketingComponent from './components/MarketingComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingComponent />
+    </div>
+  );
+};
+
+export default App;
